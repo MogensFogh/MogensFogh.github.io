492103b2ce8956b62c24e81e53830762f21095f8
Mogens Fogh <mfogh@microsoft.com>
2020-04-13 14:43:55 +0200

    Template app

---
+++ precache-manifest.761f537504e649a52820f4f7f56de2dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03c3465e5d161e3da89346da8c00795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "692e28eecbf14a7761fc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c284538373647357b06",
+    "url": "/static/js/2.2ae78ee8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ae78ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692e28eecbf14a7761fc",
+    "url": "/static/js/main.7f2f20e4.chunk.js"
+  },
+  {
+    "revision": "5382678bcb18bced5118",
+    "url": "/static/js/runtime-main.7dd0ba52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
